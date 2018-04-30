ea94eb4e1f345821d580910823b1a2bb5a33a6e4
skellep193 <skellep193@gmail.com>
2018-04-30 15:16:55 -0400

    Add files via upload

--- GCodeHashLib/GCodeHashLib.py
+++ GCodeHashLib/GCodeHashLib.py
@@ -14,14 +14,64 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import hashlib
-import os.stat, os.path
+#   Author: Patrick Skelley <pskelley@albany.edu>, Jenny Chen <>, University at Albany (SUNY), Albany NY
+#   Purpose: Library embeds MD5 and SHA256 checksums within gcode files for pre-print validation purposes
+#
+#   Revision History
+#   1.0: Initial Release - 04/29/2018
 
-class GCodeMD5Checksum:
+import hashlib
+import os.stat, os.path,  os.rename
+
+#   Author: Patrick Skelley, Jenny Chen
+#   Parent Class for algorithm specific hash classes
+class GCodeChecksum:
+    
+    #Author: Patrick Skelley
+    # Purpose: Verifies the hash embedded within the gcode file matches that of the hash generated against the file content
+    def VerifySignature(self):
+        with open(self.filename,  "r") as f:
+            firstLine = self.readline()
+            tokens = firstLine.split("=")
+            existingHash = tokens[1]
+            hashAlg = tokens[2]
+            
+        if hashAlg.endwith("MD5"):
+            hashOp = hashlib.md5()
+        elif hashAlg.endswith("SHA256"):
+            hashOp = hashlib.sha256()
+        else:
+            raise "Unknown hash algorithm"
+            
+        with open(self.filename, "rb") as f:
+            byte = f.read(1)
+            while byte.decode("utf-8") != '\x0A':
+               byte = f.read(1)
+          
+            for block in iter(lambda: f.read(4096), b""):
+                hashOp.update(block)
+                hash = hashOp.hexdigest()
+        return(hash==existingHash)
+
+    # Author: Jenny Chen
+    # Purpose: Performs checks on provided file path. Confirms existence, extension, and content.
+    def ValidateFileInput(checkname,  extension):
+        if os.path.exists(checkname) != 1:
+                        raise LookupError("File not found")
+        if checkname.endswith(extension) != 1:
+                        raise LookupError("ncorrect file extension")
+        if os.stat(checkname).st_size == 0:
+                        raise LookupError("File is empty")
+        return(True)
+
+#   Author: Patrick Skelley
+#   Purpose: Class for generating and embedding MD5 checksum in g-code files
+class GCodeMD5Checksum(GCodeChecksum):
     def __init__(self,  filename):
-        ValidateFileInput(filename)
+        GCodeChecksum.ValidateFileInput(filename, ".gcode")
         self.filename = filename
-        
+    
+    # Purpose: Generates and embeds MD5 signature
     def ApplySignature(self):
         md5 = hashlib.md5()
         with open(self.filename, "rb") as f:
@@ -34,28 +84,19 @@ class GCodeMD5Checksum:
             print(contents)
             contents.insert(0,";GCODEHASHLIB_MD5=" + hash + "\n") 
             
-        with open(self.filename,  "w",encoding='utf-8') as f:
+        with open(os.rename(self.filename, self.filename + "_MD5"),  "w",encoding='utf-8') as f:
             f.writelines(contents)
-        
-    def VerifySignature(self, md5hash):
-        md5 = hashlib.md5()
-        with open(self.filename, "rb") as f:
-            byte = f.read(1)
-            while byte.decode("utf-8") != '\x0A':
-               byte = f.read(1)
-          
-            for block in iter(lambda: f.read(4096), b""):
-                md5.update(block)
-                hash = md5.hexdigest()
-        
-        return(hash==md5hash)
-        
-class GCodeSHA256Checksum:
+
+#   Author: Patrick Skelley
+#   Purpose: Class for generating and embedding SHA256 checksum in g-code files
+class GCodeSHA256Checksum(GCodeChecksum):
     def __init__(self,  filename):
-        ValidateFileInput(filename)
+        GCodeChecksum.ValidateFileInput(filename, ".gcode")
         self.filename = filename
-        
+    
+    # Purpose: Generates and embeds SHA256 signature
     def ApplySignature(self):
+        
         sha256 = hashlib.sha256()
         with open(self.filename, "rb") as f:
             for block in iter(lambda: f.read(4096), b""):
@@ -67,28 +108,5 @@ class GCodeSHA256Checksum:
             print(contents)
             contents.insert(0,";GCODEHASHLIB_SHA256=" + hash + "\n") 
             
-        with open(self.filename,  "w",encoding='utf-8') as f:
+        with open(os.rename(self.filename, self.filename + "_SHA256"),  "w",encoding='utf-8') as f:
             f.writelines(contents)
-        
-    def VerifySignature(self, sha256hash):
-        sha256 = hashlib.sha256()
-        with open(self.filename, "rb") as f:
-            byte = f.read(1)
-            while byte.decode("utf-8") != '\x0A':
-               byte = f.read(1)
-          
-            for block in iter(lambda: f.read(4096), b""):
-                sha256.update(block)
-                hash = sha256.hexdigest()
-        
-        return(hash==sha256hash)
-        
-
-def ValidateFileInput(checkname):
-    if os.path.exists(checkname) != 1:
-                    raise LookupError("File not found")
-    if checkname.endswith(".gcode") != 1:
-                    raise LookupError("ncorrect file extension")
-    if os.stat(checkname).st_size == 0:
-                    raise LookupError("File is empty")
-    return(True)
